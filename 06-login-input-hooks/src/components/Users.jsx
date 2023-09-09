8747886b265d1532a21fb2c8dfad1d27fadd9ba9
kohdc <kohdc1723@gmail.com>
2023-09-09 16:35:12 -0700

    input hooks complete

---
+++ 06-login-input-hooks/src/components/Users.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const Users = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [users, setUsers] = useState();
+    const axiosPrivate = useAxiosPrivate();
+
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getUsers = async () => {
+            try {
+                const response = await axiosPrivate.get("/users", { signal: controller.signal });
+                console.log(response.data);
+                isMounted && setUsers(response.data);
+            } catch (err) {
+                console.error(err);
+                navigate("/login", { state: { from: location }, replace: true });
+            }
+        }
+
+        getUsers();
+
+        // cleanup function
+        return () => {
+            isMounted = false;
+            controller.abort();
+        };
+    }, []);
+
+    return (
+        <article>
+            <h2>Users List</h2>
+            {users?.length ? (
+                <ul>
+                    {users.map((user, i) => (
+                        <li key={i}>{user?.username}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>no users to display</p>
+            )}
+        </article>
+    );
+};
+
+export default Users;
